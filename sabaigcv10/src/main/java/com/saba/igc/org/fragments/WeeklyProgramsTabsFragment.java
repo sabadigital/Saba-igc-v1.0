600ecdee0fa47ab4eb32243054a5b0b55948e2bd
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2015-06-17 13:08:44 -0700

    Attempt to add ViewPager in WeeklySchedule Fragment.
    This code is not being used.

--- sabaigcv10/src/main/java/com/saba/igc/org/fragments/WeeklyProgramsTabsFragment.java
+++ sabaigcv10/src/main/java/com/saba/igc/org/fragments/WeeklyProgramsTabsFragment.java
@@ -1,8 +1,8 @@
 package com.saba.igc.org.fragments;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -124,4 +124,40 @@ public class WeeklyProgramsTabsFragment extends Fragment {
 
         return view;
     }
+
+//    public static class MyPagerAdapter extends FragmentPagerAdapter {
+//        private static int NUM_ITEMS = 3;
+//
+//        public MyPagerAdapter(FragmentManager fragmentManager) {
+//            super(fragmentManager);
+//        }
+
+//        // Returns total number of pages
+//        @Override
+//        public int getCount() {
+//            return NUM_ITEMS;
+//        }
+//
+//        // Returns the fragment to display for that page
+//        @Override
+//        public Fragment getItem(int position) {
+//            switch (position) {
+//                case 0: // Fragment # 0 - This will show FirstFragment
+//                    return FirstFragment.newInstance(0, "Page # 1");
+//                case 1: // Fragment # 0 - This will show FirstFragment different title
+//                    return FirstFragment.newInstance(1, "Page # 2");
+//                case 2: // Fragment # 1 - This will show SecondFragment
+//                    return SecondFragment.newInstance(2, "Page # 3");
+//                default:
+//                    return null;
+//            }
+//        }
+//
+//        // Returns the page title for the top indicator
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            return "Page " + position;
+//        }
+//
+//    }
 }
