9426ebf0ef4b1db88daa84615dc169a9d73fbbc0
Syed Naqvi <aftabnaqvi2003@hotmail.com>
2015-07-06 03:56:31 -0700

    Added Maps key in ManifestFile.
     - Somehow it was not working via google_maps_api.xml
    -Added the new Splash Screen. Also, getting the “Weekly Schedule” via
    network call on every fresh launch.

--- sabaigcv10/src/main/java/com/saba/igc/org/activities/SplashScreenActivity.java
+++ sabaigcv10/src/main/java/com/saba/igc/org/activities/SplashScreenActivity.java
@@ -3,14 +3,23 @@ package com.saba.igc.org.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import com.saba.igc.org.R;
+import com.saba.igc.org.application.SabaApplication;
+import com.saba.igc.org.listeners.SabaServerResponseListener;
+import com.saba.igc.org.models.DailyProgram;
+import com.saba.igc.org.models.SabaProgram;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
 /**
  * Created by Syed Aftab Naqvi on 1/18/15.
  */
 
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends Activity implements SabaServerResponseListener {
 
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 1500;
@@ -20,23 +29,81 @@ public class SplashScreenActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreenActivity.this, MainActivity1.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        // on each fresh launch, try to pull the fresh data from server.
+        SabaApplication.getSabaClient().getWeeklyPrograms(this);
+//        new Handler().postDelayed(new Runnable() {
+//
+//            /*
+//             * Showing splash screen with a timer. This will be useful when you
+//             * want to show case your app logo / company
+//             */
+//
+//            @Override
+//            public void run() {
+//                // This method will be executed once the timer is over
+//                // Start your app main activity
+//                Intent i = new Intent(SplashScreenActivity.this, MainActivity1.class);
+//                startActivity(i);
+//
+//                // close this activity
+//                finish();
+//            }
+//        }, SPLASH_TIME_OUT);
     }
+
+    public void processJsonObject(String programName, JSONObject response) {
+        // we received an error... may be internet coonection not found. Showing empty activity.
+        Intent i = new Intent(SplashScreenActivity.this, MainActivity1.class);
+        startActivity(i);
+
+        // close this activity
+        finish();
+    }
+
+    public void processJsonObject(String programName, JSONArray response){
+        if(response != null) {
+            List<SabaProgram> programs = null;
+            if (programName != null && programName.compareToIgnoreCase("Weekly Programs") == 0) {
+                // parse weekly programs differently....
+                List<List<DailyProgram>> weeklyPrograms = DailyProgram.fromJSONArray(programName, response);
+                programs = SabaProgram.fromWeeklyPrograms(programName, weeklyPrograms);
+
+                addAll(programs);
+                addAllWeeklyPrograms(weeklyPrograms);
+            }
+        }
+
+        // our database is ready with if there is no error, lets start the new activity.
+        Intent i = new Intent(SplashScreenActivity.this, MainActivity1.class);
+        startActivity(i);
+
+        // close this activity
+        finish();
+    }
+
+    // Delegate the adding to the internal adapter. // most recommended approach... minimize the code...
+    private void addAll(List<SabaProgram> programs){
+        // delete existing records if there is any. We don't want to keep duplicate entries.
+        SabaProgram.deleteSabaPrograms("Weekly Programs");
+
+        // save new/latest programs.
+        for(final SabaProgram program : programs){
+            program.saveProgram();
+        }
+    }
+
+    // delete old data from the WeeklyProgram table and then save all newly retrieved weekly Programs.
+    private void addAllWeeklyPrograms(List<List<DailyProgram>> programs){
+        // delete existing records. We don't want to keep duplicate/old entries.
+        DailyProgram.deletePrograms();
+
+        // save new/latest programs.
+        for(final List<DailyProgram> dailyPrograms : programs){
+            for(final DailyProgram program : dailyPrograms){
+                program.saveProgram();
+            }
+        }
+    }
+
 }
